40bdf25aa7eb7b4f2e89d39c27a41eb55a170777
David Chung <dchung@dchung-macbookpro.local>
2010-04-12 18:52:49 -0700

    New project layout for ib-client/ api

---
+++ cpp-ib/test/ib/client/ClientTest.cpp
@@ -0,0 +1,15 @@
+
+#include "ib/mocks/MockEWrapper.h"
+#include "ib/mocks/MockEClient.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace {
+
+TEST(SimpleTest2, HelloWorld2) {
+  EXPECT_EQ(2, 1+1);
+}
+
+} // namespace
+
---
+++ cpp-ib/test/ib/client/SimpleClientTest.cpp
@@ -0,0 +1,12 @@
+
+#include <ib/client/SimpleClient.h>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+namespace {
+
+TEST(SimpleTest1, HelloWorld) {
+  EXPECT_EQ(2, 1+1);
+}
+
+} // namespace
