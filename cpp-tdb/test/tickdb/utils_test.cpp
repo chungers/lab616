b7aaef6ec9c421256996017700a60e6bb75fe856
David <davidc616@gmail.com>
2010-07-07 01:56:14 -0700

    Prototyping db key encoding.

--- cpp-tdb/test/tickdb/utils_test.cpp
+++ cpp-tdb/test/tickdb/utils_test.cpp
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <iostream>
+#include <sstream>
 #include <vector>
 #include <unistd.h>
 
@@ -51,6 +52,92 @@ ptime Convert_UTC(adjustor adj, ptime local_time)
   return adj.local_to_utc(local_time);
 }
 
+TEST(UtilsTest, TestDateTimeFunctions)
+{
+  using namespace boost::posix_time;
+
+  ptime now_utc = microsec_clock::universal_time();
+  ptime now_local = microsec_clock::local_time();
+
+  // UTC is ahead of Pacific.
+  EXPECT_GE(now_utc, now_local);
+
+  // We reasonably expect accuracy up to the seconds.
+  EXPECT_EQ(us_pacific::local_to_utc(now_local).time_of_day().seconds(),
+            now_utc.time_of_day().seconds());
+
+  EXPECT_EQ(us_pacific::utc_to_local(now_utc).time_of_day().seconds(),
+            now_local.time_of_day().seconds());
+
+  time_duration offset1 = now_local - now_utc; // about 7 hours behind.
+
+  cout << "offset1 = " << offset1 << endl;
+
+  ptime epoch(date(1970, 1, 1)); // UTC start
+  time_duration t = now_utc - epoch;
+  time_duration t2 = now_local - epoch;
+
+  time_duration offset2 = t2 - t;
+
+  cout << "offset2 = " << offset2 << endl;
+
+  EXPECT_EQ(offset1.total_milliseconds(), offset2.total_milliseconds());
+
+  EXPECT_EQ(8, sizeof(t.total_microseconds())); // 8 bytes for microseconds.
+
+}
+
+// http://stackoverflow.com/questions/1466756/c-equivalent-of-java-bytebuffer
+template <typename T>
+std::stringstream& put( std::stringstream& str, const T& value )
+{
+  union coercion { T value; char   data[ sizeof ( T ) ]; };
+  coercion    c;
+  c.value = value;
+  str.write ( c.data, sizeof ( T ) );
+  return str;
+}
+
+template <typename T>
+std::stringstream& get( std::stringstream& str, T& value )
+{
+  union coercion { T value; char   data[ sizeof ( T ) ]; };
+  coercion    c;
+  c.value = value;
+  str.read ( c.data, sizeof ( T ) );
+  value = c.value;
+  return str;
+}
+
+TEST(UtilsTest, TestEncodeKey)
+{
+  using namespace boost::posix_time;
+  ptime now_utc = microsec_clock::universal_time();
+  ptime epoch(date(1970, 1, 1)); // UTC start
+  time_duration t = now_utc - epoch;
+
+  // Encode database key.
+  std::stringstream bytes;
+  string symbol("AAPL");
+  string::iterator itr;
+  for (itr = symbol.begin(); itr < symbol.end(); itr++) {
+    put(bytes, *itr);
+  }
+  put(bytes, t.total_microseconds());
+  cout << "ts = " << t.total_microseconds() << endl;
+
+  const char* p = bytes.str().c_str();  // kyotocabinet key buffer type
+  cout << "symbol = " << string(p, 4) << endl;
+
+  EXPECT_EQ(symbol, string(p, 4));
+
+  std::stringstream ts_bytes(string(&p[4], 8));
+  uint64_t ts;
+  get(ts_bytes, ts);
+  cout << "ts = " << ts << endl;
+
+  EXPECT_EQ(t.total_microseconds(), ts);
+}
 
 TEST(UtilsTest, TestTimeFunctions)
 {
