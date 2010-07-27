83940cc8c65c2fdae267f897376d189493820585
David <davidc616@gmail.com>
2010-07-27 08:57:25 -0700

    Prototyping / building TickDb: marshaller template.

--- cpp-tdb/src/tickdb/tickdb.hpp
+++ cpp-tdb/src/tickdb/tickdb.hpp
@@ -11,10 +11,48 @@
 #include <boost/filesystem/operations.hpp>
 
 #include <glog/logging.h>
+#include "tickdb/tickdb_format.pb.h"
 
 namespace tickdb {
 
 typedef uint64_t Timestamp;
+
+/**
+ * Protobuffer message marshaller.
+ * C is a type code, and T is the proto message type.
+ */
+template <int C, typename T>
+class Marshaller
+{
+ public:
+  ~Marshaller() {}
+
+  /** @return The type code. */
+  int TypeCode() { return C; }
+
+  /**
+   * @param input The proto message
+   * @param output The output mutable string.
+   * @returns True if success.
+   */
+  bool SerializeToString(const T& input, std::string* output)
+  {
+    return input.SerializeToString(output);
+  }
+
+  /**
+   * @param input The const string buffer.
+   * @param output The mutable message to merge results to.
+   * @returns True if success.
+   */
+  bool ParseFromString(const std::string& input, T* output)
+  {
+    return output.ParseFromString(input);
+  }
+};
+
+typedef Marshaller<0, tickdb::file::Payload> PayloadMarshaller;
+
 
 class TickDbInterface
 {
