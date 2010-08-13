d239ba7236a576e77877c2634ece7b7a4c4e8c07
David <davidc616@gmail.com>
2010-08-13 03:30:51 -0700

    Added missing test file.

---
+++ cpp-ib/test/ib/backplane_test.cpp
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <boost/algorithm/string.hpp>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+
+using namespace std;
+
+namespace {
+
+TEST(BackPlaneTest, TestSignals)
+{
+}
+
+
+} // namespace
