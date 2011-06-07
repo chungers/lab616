210873106953ffc23da67264b949f787dc6eb34d
David <davidc616@gmail.com>
2011-06-07 01:08:50 -0700

    Lua / luabind prototype continues. Inclusion of a REPL using ilua

---
+++ cpp-ib/lua/test.lua
@@ -0,0 +1,4 @@
+io.write("Please enter your name: ")
+name = io.read() -- read input from user
+show ("Hi " .. name .. ", enjoy hacking with Lua");
+
--- cpp-ib/prototype/luabind_main.cpp
+++ cpp-ib/prototype/luabind_main.cpp
@@ -7,26 +7,32 @@ extern "C"
 
 #include <iostream>
 #include <luabind/luabind.hpp>
+#include <glog/logging.h>
+#include <gflags/gflags.h>
+
+
+DEFINE_string(src, "ilua.lua", "Source to run.");
+
 
 void greet()
 {
-    std::cout << "hello world!\n";
+  std::cout << "hello world!\n";
 }
 
 using namespace std;
 
 extern "C" int init(lua_State* L)
 {
-    using namespace luabind;
+  using namespace luabind;
 
-    open(L);
+  open(L);
 
-    module(L)
-    [
-        def("greet", &greet)
-    ];
+  module(L)
+      [
+          def("greet", &greet)
+       ];
 
-    return 0;
+  return 0;
 }
 
 
@@ -34,23 +40,40 @@ void print_hello(int number) {
   cout << "hello world " << number << endl;
 }
 
-int main() {
+void message(const string& message) {
+  cout << "The message is: " << message << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+
+  google::SetUsageMessage("Prototype for the mongoose httpd.");
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+
   // Create a new lua state
-  lua_State *myLuaState = lua_open();
+  lua_State *L = lua_open();
+
+  luaL_openlibs(L);
 
   // Connect LuaBind to this lua state
-  luabind::open(myLuaState);
+  luabind::open(L);
 
   // Add our function to the state's global scope
-  luabind::module(myLuaState) [
-      luabind::def("print_hello", print_hello)
-                               ];
+  luabind::module(L) [
+      luabind::def("print_hello", print_hello),
+      luabind::def("show", message)
+                      ];
+
+  LOG(INFO) << "Starting interpreter." << std::endl;
 
   // Now call our function in a lua script
-  luaL_dostring(
-      myLuaState,
-      "print_hello(123)\n"
+  luaL_dostring(L,
+                "print_hello(123)\n"
                 );
 
-  lua_close(myLuaState);
+  LOG(INFO) << "Try to run file " << FLAGS_src.c_str() << std::endl;
+
+  luaL_dofile(L, FLAGS_src.c_str());
+
+  lua_close(L);
 }
